6b8609002d83196caeece2582b92b0bdcbafc658
Siddhesh Kamble <siddhesh.kamble7867@gmail.com>
2021-06-24 19:06:14 +0530

    fadeIn effect

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
 const popUp = document.querySelector('.pop_up');
 const shareButton = document.querySelector('.share_button');
 const userInfo = document.querySelector('.user_info');
-const svg = document.querySelector('#svg');
+const svg = document.querySelector('.svg-grey');
 
 console.log('Hi');
-popUp.classList.add('none');
 
 const popUpDisplay = () => {
-  if (popUp.style.display == 'flex') {
-    popUp.style.display = 'none';
-    shareButton.classList.remove('select-back');
-    svg.classList.remove('svg-white');
-  } else {
-    popUp.style.display = 'flex';
-    shareButton.classList.add('select-back');
-    svg.classList.add('svg-white');
-  }
+  // if (popUp.style.display == 'flex') {
+  //   popUp.style.display = 'none';
+  //   shareButton.classList.remove('select-back');
+  //   svg.classList.remove('svg-white');
+  // } else {
+  //   popUp.style.display = 'flex';
+  //   shareButton.classList.add('select-back');
+  //   svg.classList.add('svg-white');
+  // }
+
+  popUp.classList.toggle('show');
+  shareButton.classList.toggle('active');
+  svg.classList.toggle('svg-white');
 };
 
 shareButton.addEventListener('click', popUpDisplay);
